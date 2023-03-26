79435242262d10235b8ad5f80869430b29878fa1
= <=>
2023-03-26 17:34:34 +0100

    The app properties file has been updated and the filter class has fully been implemented alongside the service class that is used to implement the respective methods in the filter class.

--- src/main/java/org/flexicode/hitechschool/filter/AppJwtAuthenticationFilter.java
+++ src/main/java/org/flexicode/hitechschool/filter/AppJwtAuthenticationFilter.java
@@ -5,17 +5,44 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+@RequiredArgsConstructor
 
 public class AppJwtAuthenticationFilter extends OncePerRequestFilter {
+    private final AppJwtService appService;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-
+       String authToken = request.getHeader("Authorization");
+       String username = "";
+       String jwtToken = "";
+       if(authToken == null || !authToken.startsWith("Bearer ")){
+           filterChain.doFilter(request, response);
+           return;
+       }
+       jwtToken = authToken.substring(7);
+       username = appService.extractUsername(jwtToken);
+       if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+           UserDetails user =  this.userDetailsService.loadUserByUsername(username);
+           if(appService.isUserValid(jwtToken, user)){
+               UsernamePasswordAuthenticationToken authenticationToken =
+                       new UsernamePasswordAuthenticationToken(username,null,user.getAuthorities());
+               authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+               SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+           }
+           filterChain.doFilter(request, response);
+       }
     }
 }
--- src/main/java/org/flexicode/hitechschool/users/UserRepository.java
+++ src/main/java/org/flexicode/hitechschool/users/UserRepository.java
@@ -1,9 +1,10 @@
 package org.flexicode.hitechschool.users;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends CrudRepository<Users, Long> {
     public Optional<Users> findByUsername(String username);
 }
